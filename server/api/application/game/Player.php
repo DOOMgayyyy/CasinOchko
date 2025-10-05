7831e23d8ce590b5b93ba8b2c8ec6d4031cdd394
Petr Kukushkin <gleb.kha3412@gmail.com>
2025-10-05 16:26:05 +0300

    Update player new FUN

--- server/api/application/game/Player.php
+++ server/api/application/game/Player.php
@@ -9,7 +9,7 @@ class Player
     public $balance;
     public $currentBet = 0;
     public $splitHands = []; //для сплита рука
-    public $activeHandIndex = 0; // Индекс активной руки
+    public $activeHandIndex = 0; //индекс активной руки
 
     public function __construct($id, $balance = 1000) //заглушка 1000 баланс 
     {
@@ -20,17 +20,52 @@ class Player
         $this->activeHandIndex = 0;
     }
 
-    //добавление карты в руку
+    //получение текущую активную руку
+    private function getCurrentHand()
+    {
+        if (!empty($this->splitHands)) {
+            return $this->splitHands[$this->activeHandIndex];
+        }
+        return $this->cards;
+    }
+
+    //получение ссылки текущей игры для изменений
+    private function &getCurrentHandRef()
+    {
+        if (!empty($this->splitHands)) {
+            return $this->splitHands[$this->activeHandIndex];
+        }
+        return $this->cards;
+    }
+
+    //добавление карты в текущую активную руку
     public function addCard(Card $card)
     {
-        $this->cards[] = $card;
+        $currentHand = &$this->getCurrentHandRef();
+
+        //5карт
+        if (count($currentHand) >= 5) {
+            return $this;
+        }
+
+        $currentHand[] = $card;
         return $this;
     }
 
-    //расчет очков руки
+    //расчет очков текущей активной руки
     public function getScore()
     {
-        return gameLogic::calculateHandScore($this->cards);
+        $currentHand = $this->getCurrentHand();
+        return gameLogic::calculateHandScore($currentHand);
+    }
+
+    //получение счета конкрутной руки
+    public function getHandScore($handIndex = null)
+    {
+        if ($handIndex !== null && isset($this->splitHands[$handIndex])) {
+            return gameLogic::calculateHandScore($this->splitHands[$handIndex]);
+        }
+        return $this->getScore();
     }
 
     //делаем ставку если хватает денег. amount - сумма ставки 
@@ -51,7 +86,7 @@ class Player
     }
 
     //сплит
-    public function split()
+    public function split($deck)
     {
         //проверка возможен ли сплит
         if (count($this->cards) != 2 || $this->cards[0]->rank !== $this->cards[1]->rank) {
@@ -70,15 +105,56 @@ class Player
         $hand2 = [$this->cards[1]];
 
         //добавляем по одной карте в каждую руку
-        $hand1[] = $deck->drawCard();
-        $hand2[] = $deck->drawCard();
+        $hand1[] = $deck->draw();
+        $hand2[] = $deck->draw();
 
-        //очищаем основную руку
-        $this->cards = [];
+        //сохраняем руки в splitHands
         $this->splitHands = [$hand1, $hand2];
         $this->activeHandIndex = 0;
 
+        //очищаем основную руку
+        $this->cards = [];
+
         return true;
+    }
+
+    //смена руки
+    public function nextHand()
+    {
+        if (!empty($this->splitHands) && $this->activeHandIndex < count($this->splitHands) - 1) {
+            $this->activeHandIndex++;
+            return true;
+        }
+        return false;
+    }
+
+    //проверка на еще 1 руку для игры
+    public function hasMoreHands()
+    {
+        return !empty($this->splitHands) && $this->activeHandIndex < count($this->splitHands) - 1;
+    }
+
+    //получение количесвто рук
+    public function getHandCount()
+    {
+        if (!empty($this->splitHands)) {
+            return count($this->splitHands);
+        }
+        return 1;
+    }
+
+    //получение руки по индексу
+    public function getHand($index = null)
+    {
+        if ($index === null) {
+            return $this->getCurrentHand();
+        }
+
+        if (!empty($this->splitHands) && isset($this->splitHands[$index])) {
+            return $this->splitHands[$index];
+        }
+
+        return $this->cards;
     }
 }
 
