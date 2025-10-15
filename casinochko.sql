1894de4db84ae9ec77a130fab3efb947acf7be4f
DOOMgayyyy <misha.samarin.00@bk.ru>
2025-10-16 00:36:27 +0400

    Update casinochko.sql

--- casinochko.sql
+++ casinochko.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Окт 08 2025 г., 17:38
+-- Время создания: Окт 15 2025 г., 23:34
 -- Версия сервера: 8.0.30
 -- Версия PHP: 7.2.34
 
@@ -24,6 +24,61 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` bigint UNSIGNED NOT NULL,
+  `room_id` bigint UNSIGNED NOT NULL,
+  `user_id` bigint UNSIGNED NOT NULL,
+  `message` text NOT NULL,
+  `created` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Таблица сообщений в чате';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `message_hashes`
+--
+
+CREATE TABLE `message_hashes` (
+  `id` bigint UNSIGNED NOT NULL,
+  `room_id` bigint UNSIGNED NOT NULL,
+  `hash` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Таблица хэшей сообщений';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `id` bigint UNSIGNED NOT NULL,
+  `type` enum('open','private') NOT NULL,
+  `status` enum('playing','closed') NOT NULL,
+  `current_member_id` bigint UNSIGNED DEFAULT NULL,
+  `private_code` int DEFAULT NULL,
+  `hash` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Таблица комнат';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `room_members`
+--
+
+CREATE TABLE `room_members` (
+  `id` bigint UNSIGNED NOT NULL,
+  `room_id` bigint UNSIGNED NOT NULL,
+  `user_id` bigint UNSIGNED NOT NULL,
+  `bet` int DEFAULT '0',
+  `types` tinyint(1) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Таблица членов комнат';
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `users`
 --
 
@@ -32,35 +87,51 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `balance` int NOT NULL DEFAULT '5000',
-  `token` varchar(255) DEFAULT NULL
+  `balance` int DEFAULT '5000',
+  `token` varchar(255) DEFAULT NULL,
+  `total_played` int DEFAULT '0',
+  `total_win` int DEFAULT '0',
+  `total_balance` int DEFAULT '5000'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Дамп данных таблицы `users`
---
-
-INSERT INTO `users` (`id`, `email`, `password`, `name`, `balance`, `token`) VALUES
-(1, 'vasya@gmail.com', '6504641b0e9392df70c83029d5f6068d', 'Vasya Pupkin', 0, NULL),
-(3, 'masha@ya.ru', '317288ae363ed746c29344a47ae7969e', 'Masha Ivanova', 0, 'ce7a363c59b9d1a751d33127e6a0b6ca'),
-(4, 'huy@gmail.com', '123456', 'Suka Blyat', 0, NULL),
-(6, 'JollyGolf@gmail.com', '$2y$10$jW28bFt8v.1mBOJPI77fo.gMcYLlsxTUlcbxxq5fPvJ6P1GmMVPQS', 'Shevcov', 0, 'd4b6b2c8a77f458fd1a5f2eebdf1829c'),
-(7, 'joiuyt@gmail.com', '25d55ad283aa400af464c76d713c07ad', 'HuySuka', 0, '90111b645395ced6309199874641d0a2'),
-(8, 'dev@gmail.com', 'a320abd2b1bfc8d7350b48ee089b75c3', 'devops', 0, 'e7b3aef3eda1dd583ca45d6d2699dcff'),
-(9, 'joiuybt@gmail.com', 'c88cef86d0d26225d9fc58ee78348a02', 'HuySukas', 0, '696b5a3e654d316d7a4cbfcd61b2bf31'),
-(10, 'jinglic@gmail.com', '25d55ad283aa400af464c76d713c07ad', 'DOOMgay', 5000, NULL),
-(11, 'suaknah@gmail.com', '25d55ad283aa400af464c76d713c07ad', 'DODEP2025', 5000, 'f173d9b6521b378fb5d9c62e4ff7f221');
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
+-- Индексы таблицы `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `room_id` (`room_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Индексы таблицы `message_hashes`
+--
+ALTER TABLE `message_hashes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `room_id` (`room_id`);
+
+--
+-- Индексы таблицы `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `room_members`
+--
+ALTER TABLE `room_members`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_user_in_room` (`room_id`,`user_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
@@ -68,10 +139,58 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `message_hashes`
+--
+ALTER TABLE `message_hashes`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `room_members`
+--
+ALTER TABLE `room_members`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `message_hashes`
+--
+ALTER TABLE `message_hashes`
+  ADD CONSTRAINT `message_hashes_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`) ON DELETE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `room_members`
+--
+ALTER TABLE `room_members`
+  ADD CONSTRAINT `room_members_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `room_members_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
