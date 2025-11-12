2807b7264d5f743b6405803514ffe6679b71ce2a
DOOMgayyyy <misha.samarin.00@bk.ru>
2025-11-12 23:00:20 +0400

    Исправление регистрации
    откат к старому варианту регистрации

--- client/src/services/server/Server.ts
+++ client/src/services/server/Server.ts
@@ -87,15 +87,17 @@ class Server {
 
 
     async registration(email: string, password: string, name: string): Promise<boolean> {
-        // Убираем passHash = md5(password)
-        // Отправляем сырой password
-        const user = await this.request<TUser>('registration', { email, password, name });
+        const passHash = md5(password);
+        // 1. Ожидаем от сервера полный объект пользователя (TUser)
+        const user = await this.request<TUser>('registration', { email, password: passHash, name });
 
+        // 2. Если пользователь успешно создан и получен...
         if (user) {
+            // 3. ...сохраняем его данные в store, чтобы он сразу вошел в систему
             this.store.setUser(user);
             return true;
         }
-        
+        // 4. Если что-то пошло не так, возвращаем false
         return false;
     }
 
