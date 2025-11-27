a389ebf986aa1f649d9f8fc62f8ba52ad8f7c63d
DOOMgayyyy <misha.samarin.00@bk.ru>
2025-11-27 20:45:40 +0400

    Create Player.php

---
+++ server/api/application/game/Player.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Класс для игровой логики и получения информации о комнате
+ */
+class GameLogic
+{
+    private $db;
+
+    function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+
+    public function makeBet()
+    {
+
+    }
+    public function getCard()
+    {
+
+    }
+    public function pass()
+    {
+
+    }
+
+}
