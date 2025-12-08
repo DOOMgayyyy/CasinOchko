126e24b8cd8ff6fa249e27a1af888d6cd43d4df2
DOOMgayyyy <misha.samarin.00@bk.ru>
2025-12-08 09:08:55 +0400

    Update casinochko.sql

--- casinochko.sql
+++ casinochko.sql
@@ -3,13 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Дек 04 2025 г., 11:54
+-- Время создания: Дек 08 2025 г., 08:07
 -- Версия сервера: 8.0.30
 -- Версия PHP: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -19,6 +20,73 @@ SET time_zone = "+00:00";
 --
 -- База данных: `casinochko`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `messagehashes`
+--
+
+CREATE TABLE `messagehashes` (
+  `id` bigint UNSIGNED NOT NULL,
+  `roomid` bigint UNSIGNED NOT NULL,
+  `hash` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Таблица хэшей для чата';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` bigint UNSIGNED NOT NULL,
+  `roomid` bigint UNSIGNED NOT NULL,
+  `userid` bigint UNSIGNED NOT NULL,
+  `message` text NOT NULL,
+  `created` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Таблица сообщений в чате';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `roommembers`
+--
+
+CREATE TABLE `roommembers` (
+  `id` bigint UNSIGNED NOT NULL,
+  `roomid` bigint UNSIGNED NOT NULL,
+  `userid` bigint UNSIGNED NOT NULL,
+  `status` varchar(50) NOT NULL DEFAULT 'spectator',
+  `bet` int DEFAULT '0',
+  `cards` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '' COMMENT 'HEX строка карт игрока'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Таблица участников комнат';
+
+--
+-- Дамп данных таблицы `roommembers`
+--
+
+INSERT INTO `roommembers` (`id`, `roomid`, `userid`, `status`, `bet`, `cards`) VALUES
+(128, 70, 4, 'spectator', 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `id` bigint UNSIGNED NOT NULL,
+  `type` enum('open','private') NOT NULL,
+  `status` enum('waiting','waiting_for_bets','playing','closed') NOT NULL DEFAULT 'waiting',
+  `current_member_id` bigint UNSIGNED DEFAULT NULL,
+  `privatecode` varchar(10) DEFAULT NULL,
+  `hash` varchar(255) DEFAULT NULL,
+  `turn_start_time` datetime DEFAULT NULL,
+  `last_update` datetime DEFAULT CURRENT_TIMESTAMP,
+  `deckOfCards` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT 'Колода карт в виде строки',
+  `dealerCards` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT 'Карты дилера'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Таблица игровых комнат';
 
 -- --------------------------------------------------------
 
@@ -45,96 +113,18 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `email`, `password`, `name`, `balance`, `token`, `totalplayed`, `totalwin`, `totalbalance`) VALUES
 (2, 'yana@awsi.com', '$2y$10$yhQWfuyAW63mcy3ZksXBFOuv4Y4m2CuCoFOucVO768BAHMekzYooe', 'owerlord', 8000, '527ad0d366406ff3acf9d677ae9f72f2', 0, 0, 5000),
 (3, 'dev@dev.com', '$2y$10$hvDnmfqRDzCBShPSqWrm2.4YVxLBpS/.FKS.FG7DiBKCwjVnI0dpq', 'dev', 5000, '8a1954228142f4c6f15188d5649b560e', 0, 0, 5000),
-(4, 'vibes@vibes.com', '25d55ad283aa400af464c76d713c07ad', 'bob', 5000, '5872883649eee47883ad6ed56451b085', 0, 0, 5000),
-(5, 'vibhs@vibes.com', '25d55ad283aa400af464c76d713c07ad', 'vibe', 1000000, '2177b595a90543bed73ad4ac97b828b0', 0, 0, 1000000);
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `rooms`
---
-
-CREATE TABLE `rooms` (
-  `id` bigint UNSIGNED NOT NULL,
-  `type` enum('open','private') NOT NULL,
-  `status` enum('waiting','waiting_for_bets','playing','closed') NOT NULL DEFAULT 'waiting',
-  `current_member_id` bigint UNSIGNED DEFAULT NULL,
-  `privatecode` varchar(10) DEFAULT NULL,
-  `hash` varchar(255) DEFAULT NULL,
-  `turn_start_time` datetime DEFAULT NULL,
-  `last_update` datetime DEFAULT CURRENT_TIMESTAMP,
-  `deckOfCards` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT 'Колода карт в виде строки',
-  `dealerCards` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT 'Карты дилера'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Таблица игровых комнат';
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `roommembers`
---
-
-CREATE TABLE `roommembers` (
-  `id` bigint UNSIGNED NOT NULL,
-  `roomid` bigint UNSIGNED NOT NULL,
-  `userid` bigint UNSIGNED NOT NULL,
-  `status` varchar(50) NOT NULL DEFAULT 'spectator',
-  `bet` int DEFAULT '0',
-  `cards` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '' COMMENT 'HEX строка карт игрока'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Таблица участников комнат';
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `messages`
---
-
-CREATE TABLE `messages` (
-  `id` bigint UNSIGNED NOT NULL,
-  `roomid` bigint UNSIGNED NOT NULL,
-  `userid` bigint UNSIGNED NOT NULL,
-  `message` text NOT NULL,
-  `created` datetime DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Таблица сообщений в чате';
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `messagehashes`
---
-
-CREATE TABLE `messagehashes` (
-  `id` bigint UNSIGNED NOT NULL,
-  `roomid` bigint UNSIGNED NOT NULL,
-  `hash` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Таблица хэшей для чата';
+(4, 'vibes@vibes.com', '25d55ad283aa400af464c76d713c07ad', 'bob', 3425, 'e6024d739d9d2cbe68e273417ea055ba', 0, 0, 5000);
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
--- Индексы таблицы `users`
+-- Индексы таблицы `messagehashes`
 --
-ALTER TABLE `users`
+ALTER TABLE `messagehashes`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `token` (`token`);
-
---
--- Индексы таблицы `rooms`
---
-ALTER TABLE `rooms`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `privatecode` (`privatecode`),
-  ADD KEY `type_status` (`type`,`status`);
-
---
--- Индексы таблицы `roommembers`
---
-ALTER TABLE `roommembers`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `unique_user_in_room` (`roomid`,`userid`),
-  ADD KEY `userid` (`userid`);
+  ADD UNIQUE KEY `roomid` (`roomid`);
 
 --
 -- Индексы таблицы `messages`
@@ -145,46 +135,62 @@ ALTER TABLE `messages`
   ADD KEY `userid` (`userid`);
 
 --
--- Индексы таблицы `messagehashes`
+-- Индексы таблицы `roommembers`
 --
-ALTER TABLE `messagehashes`
+ALTER TABLE `roommembers`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `roomid` (`roomid`);
+  ADD UNIQUE KEY `unique_user_in_room` (`roomid`,`userid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Индексы таблицы `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `privatecode` (`privatecode`),
+  ADD KEY `type_status` (`type`,`status`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `token` (`token`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
--- AUTO_INCREMENT для таблицы `users`
+-- AUTO_INCREMENT для таблицы `messagehashes`
 --
-ALTER TABLE `users`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT для таблицы `rooms`
---
-ALTER TABLE `rooms`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `roommembers`
---
-ALTER TABLE `roommembers`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `messagehashes`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT для таблицы `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT для таблицы `messagehashes`
+-- AUTO_INCREMENT для таблицы `roommembers`
 --
-ALTER TABLE `messagehashes`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `roommembers`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=129;
 
+--
+-- AUTO_INCREMENT для таблицы `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
