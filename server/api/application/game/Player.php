940d87a013cbb94741d17a956a148e78a5caa071
smakgood <alexmacs938@gmail.com>
2025-12-18 21:31:57 +0400

    dropBetFixes1

--- server/api/application/game/Player.php
+++ server/api/application/game/Player.php
@@ -115,7 +115,6 @@ class Player {
                         $this->db->updateDealerCards($roomId, $dealerFullCards);
                         $this->db->updateRoomStatus($roomId, 'playing');
                         $this->calculateAndPayResults($roomId);
-                        $this->startNewRound($roomId);
                         return ['success' => true, 'dealerBlackjack' => true];
                     }
                     // Нет блэкджека - скрываем вторую карту (не сохраняем)
